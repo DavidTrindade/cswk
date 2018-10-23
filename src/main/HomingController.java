daf39fa3bfe7166480f32ce8d639e9ce8957965a
David Ferreira <d.ferreira@warwick.ac.uk>
2018-10-23 20:08:34 +0100

    Homing Controller Almost Finished

--- src/main/HomingController.java
+++ src/main/HomingController.java
@@ -15,54 +15,69 @@ public class HomingController implements IRobotController {
     public void start() {
         this.active = true;
 
-        int direction;
-        int randno;
+        while(!robot.getLocation().equals(robot.getTargetLocation()) && active) {
 
-        direction = robot.look(IRobot.EAST);
+          int direction = 0;
+          int randno;
 
-        do {
-            randno = (int) Math.round(Math.random()*3);
+          do {
 
-            // change the direction based on the random number
-            if (randno == 0)
-                direction = IRobot.LEFT;
-            else if (randno == 1)
-                direction = IRobot.RIGHT;
-            else if (randno == 2)
-                direction = IRobot.BEHIND;
-            else
-                direction = IRobot.AHEAD;
+              randno = (int)(Math.random() * 4);
 
-            // wait for a while if we are supposed to
-            if (delay > 0)
-                robot.sleep(delay);
-        } while (robot.look(IRobot.AHEAD)==IRobot.WALL);
 
-        robot.face(direction);  /* Face the direction */
+              // change the direction based on the random number
+              if (randno == 0)
+                  direction = IRobot.LEFT;
+              else if (randno == 1)
+                  direction = IRobot.RIGHT;
+              else if (randno == 2)
+                  direction = IRobot.BEHIND;
+              else
+                  direction = IRobot.AHEAD;
+
+              // wait for a while if we are supposed to
+              if (delay > 0)
+                  robot.sleep(delay);
+
+          } while (robot.look(direction)==IRobot.WALL);
+
+          robot.face(direction);  /* Face the direction */
+          robot.advance();
+
+      }
     }
 
     // this method returns 1 if the target is north of the
     // robot, -1 if the target is south of the robot, or
     // 0 if otherwise.
     public byte isTargetNorth() {
-        // TODO: Implement for Task 5
-        return 0;
+        if (this.robot.getLocation().y > this.robot.getTargetLocation().y)
+          return (byte)1;
+        else if (this.robot.getLocation().y < this.robot.getTargetLocation().y)
+          return (byte)-1;
+        else return (byte)0;
     }
 
     // this method returns 1 if the target is east of the
     // robot, -1 if the target is west of the robot, or
     // 0 if otherwise.
     public byte isTargetEast() {
-        // TODO: Implement for Task 5
-        return 0;
+      if (robot.getLocation().x < robot.getTargetLocation().x)
+        return (byte)1;
+      else if (robot.getLocation().x > robot.getTargetLocation().x)
+        return (byte)-1;
+      else return (byte)0;
     }
 
     // this method causes the robot to look to the absolute
     // direction that is specified as argument and returns
     // what sort of square there is
     public int lookHeading(int absoluteDirection) {
-        // TODO: Implement for Task 5
-        return IRobot.WALL;
+      int difference = ((robot.getHeading() - absoluteDirection) % 4);
+      if (difference < 0)
+        difference += 4;
+      return robot.look(IRobot.AHEAD + difference);
+
     }
 
     // this method determines the heading in which the robot
